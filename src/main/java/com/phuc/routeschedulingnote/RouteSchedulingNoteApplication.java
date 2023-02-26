80f2ab0d718e7d8bfb54e9984a30f5fb795b98ca
phuc16102001 <phuc16102001@gmail.com>
2023-02-26 18:43:02 +0700

    Init commit

---
+++ src/main/java/com/phuc/routeschedulingnote/RouteSchedulingNoteApplication.java
@@ -0,0 +1,13 @@
+package com.phuc.routeschedulingnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteSchedulingNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteSchedulingNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phuc/routeschedulingnote/RouteSchedulingNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phuc.routeschedulingnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteSchedulingNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
